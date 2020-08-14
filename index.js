be8da4e2bc2d4c6d07b97c1e8253d5564ccec5cb
cdsuddreth <cdsuddreth@gmail.com>
2020-08-13 20:52:31 -0400

    added env variables

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 // code away!
+require('dotenv').config();
 const express = require("express");
 const server = express();
+const cors = require("cors");
+const helmet = require("helmet");
 const logger = require("./middleware/logger");
-const posts = require("./posts/postRouter");
+// const posts = require("./posts/postRouter");
 const users = require("./users/userRouter");
-const port = 5000;
+const port = process.env.PORT;
 
 
 server.use(express.json());
 server.use(users);
+server.use(cors);
+server.use(helmet);
 
 server.get('/', (req, res) => {
     res.send('<h1>Speak, friend, and enter.</h1>')
