2e5fe48cd4cfca5b10959930f0e86135cc1ab1dc
cdsuddreth <cdsuddreth@gmail.com>
2020-08-13 21:17:52 -0400

    test

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ require('dotenv').config("./.env");
 const express = require("express");
 const server = express();
 const cors = require("cors");
-const helmet = require("helmet");
 const logger = require("./middleware/logger");
 // const posts = require("./posts/postRouter");
 const users = require("./users/userRouter");
@@ -13,7 +12,6 @@ const port = process.env.PORT || 5000;
 server.use(express.json());
 server.use(users);
 server.use(cors);
-server.use(helmet);
 
 server.get('/', (req, res) => {
     res.send('<h1>Speak, friend, and enter.</h1>')
